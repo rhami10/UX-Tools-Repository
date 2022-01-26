46b8c713898bd9eca7c5e1980832d7b774949c8c
rhami1 <hami159@gmail.com>
2022-01-26 20:55:11 +0100

    Fixed bug - for dev/live versions.

--- js/loadlinks.js
+++ js/loadlinks.js
@@ -1,9 +1,8 @@
 // OK, I can't believe you actually came snooping around.
+
 import { GetBookmarkText, GetBookmarkImageLink, HasClassIncludeParents, OpenAllLinks } from './utilities.js';
 import { url, params, hashedURL } from './globals.js';
 import { getCookie, setCookie } from './cookieStorage.js';
-
-import { urlLocal } from './test/local_values.js';
 
 var storedPasskey = getCookie('passkey');
 
@@ -47,93 +46,99 @@ function GenerateCards (passKeyUserInput) {
 
         var compositeURL;
 
-        try {
-            compositeURL = urlLocal;
-            console.log("[DEV] Using Local Test JSON");
-        } catch (error) {
+        import('/js/test/local_values.js')
+        .then((data) => {
+            
+            console.log("[DEV] Using Local Test JSON.");
+            LoadInCardsHTML(data.urlLocal);
+
+        }).catch((error) => {
+
             // Fetching Live Version of Bookmarks
-            console.log(error);
-            compositeURL = new URL(checkHash.url);
-            Object.keys(params).forEach(key => compositeURL.searchParams.append(key, params[key]))
-        }
-        
-        console.log(compositeURL);
-
-        fetch(compositeURL)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-
-                data.subcategories.forEach(subcategory => {
-
-                    $('.cards').append(
-                        '<div class="card [ is-collapsed ]">'
-                        + '<div class="card__inner [ js-expander ]">'
-                        + '<div class="card__inner__iconcircle">'
-                        + '<span class="circle"></span>'
-                        + '<i class="' + subcategory.icon + '"></i>'
-                        + '</div>'
-                        + '<div class="card__inner__text">'
-                        + '<span>' + subcategory.name + '</span>'
-                        + '</div>'
-                        + '</div>'
-                        + '<div id="expander-' + subcategory.name + '" class="card__expander">'
-                        + '<div class="button-container"><button id="button-all-'+ subcategory.name +'">Open All</button></div>'
-                        + '</div>'
-                        + '</div>'
-                    );
-
-                    document.getElementById('button-all-' + subcategory.name).addEventListener("click", () => OpenAllLinks(data.bookmarks, subcategory.name));
-                });
-
-                data.bookmarks.forEach(bookmark => {
-
-                    $('#expander-' + bookmark.subcategory).append(
-                        '<div class="bookmark">'
-                        + '<a href="' + bookmark.link + '" target="_blank">'
-                        + '<div class="bookmark__inner" style="background-image: url(' + GetBookmarkImageLink(bookmark) + ')">'
-                        + '<div class="overlay"><p>' + GetBookmarkText(bookmark) + '</p></div>'
-                        + '</div>'
-                        + '</a>'
-                        + '</div>'
-                    )
-                });
-
-                var $cell = $('.card');
-
-                // Appending outer click event to close expanded elements
-                $(document).on("click", "body", function (event) {
-                    if(!HasClassIncludeParents(event.target, 'is-expanded')) {
-                        $('.is-expanded').removeClass('is-expanded').addClass('is-collapsed');
-                        $cell.not($('is-expanded')).removeClass('is-inactive');
-                    }
-                });
-
-
-                // Appending click event and expander elements for each card
-                $cell.find('.js-expander').click(function () {
-
-                    var $thisCell = $(this).closest('.card');
-
-                    if ($thisCell.hasClass('is-collapsed')) {
-                        $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
-                        $thisCell.removeClass('is-collapsed').addClass('is-expanded');
-                    } else {
-                        $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-                        $cell.not($thisCell).removeClass('is-inactive');
-                    }
-                });
-
-            }).catch((error) => {
-                $('.cards').append(
-                    '<p style="margin: auto">An error has occured.</p>' +
-                    '<p style="margin: auto">Message: '+ error +'</p>'
-                )
-            });
+            console.log("[LIVE] Using online JSON.");
+            var compositeURL = new URL(checkHash.url);
+            Object.keys(params).forEach(key => compositeURL.searchParams.append(key, params[key]));
+            LoadInCardsHTML(compositeURL);
+            
+        });
         
         return true;
     }
 
     else { return false; }
+}
+
+function LoadInCardsHTML (jsonURL) {
+    fetch(jsonURL)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+
+            data.subcategories.forEach(subcategory => {
+
+                $('.cards').append(
+                    '<div class="card [ is-collapsed ]">'
+                    + '<div class="card__inner [ js-expander ]">'
+                    + '<div class="card__inner__iconcircle">'
+                    + '<span class="circle"></span>'
+                    + '<i class="' + subcategory.icon + '"></i>'
+                    + '</div>'
+                    + '<div class="card__inner__text">'
+                    + '<span>' + subcategory.name + '</span>'
+                    + '</div>'
+                    + '</div>'
+                    + '<div id="expander-' + subcategory.name + '" class="card__expander">'
+                    + '<div class="button-container"><button id="button-all-'+ subcategory.name +'">Open All</button></div>'
+                    + '</div>'
+                    + '</div>'
+                );
+
+                document.getElementById('button-all-' + subcategory.name).addEventListener("click", () => OpenAllLinks(data.bookmarks, subcategory.name));
+            });
+
+            data.bookmarks.forEach(bookmark => {
+
+                $('#expander-' + bookmark.subcategory).append(
+                    '<div class="bookmark">'
+                    + '<a href="' + bookmark.link + '" target="_blank">'
+                    + '<div class="bookmark__inner" style="background-image: url(' + GetBookmarkImageLink(bookmark) + ')">'
+                    + '<div class="overlay"><p>' + GetBookmarkText(bookmark) + '</p></div>'
+                    + '</div>'
+                    + '</a>'
+                    + '</div>'
+                )
+            });
+
+            var $cell = $('.card');
+
+            // Appending outer click event to close expanded elements
+            $(document).on("click", "body", function (event) {
+                if(!HasClassIncludeParents(event.target, 'is-expanded')) {
+                    $('.is-expanded').removeClass('is-expanded').addClass('is-collapsed');
+                    $cell.not($('is-expanded')).removeClass('is-inactive');
+                }
+            });
+
+
+            // Appending click event and expander elements for each card
+            $cell.find('.js-expander').click(function () {
+
+                var $thisCell = $(this).closest('.card');
+
+                if ($thisCell.hasClass('is-collapsed')) {
+                    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+                    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+                } else {
+                    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+                    $cell.not($thisCell).removeClass('is-inactive');
+                }
+            });
+
+        }).catch((error) => {
+            $('.cards').append(
+                '<p style="margin: auto">An error has occured.</p>' +
+                '<p style="margin: auto">Message: '+ error +'</p>'
+            )
+        });
 }
